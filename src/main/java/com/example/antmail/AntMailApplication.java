aaef171fdc2720108fc41ef7d85cb5860c01b929
lzfdw <1256687835@qq.com>
2025-01-15 16:37:52 +0800

    创建项目

---
+++ src/main/java/com/example/antmail/AntMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.antmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/antmail/AntMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.antmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
