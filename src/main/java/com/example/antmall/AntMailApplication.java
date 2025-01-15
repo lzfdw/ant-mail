37fba27168f5f3cbb493342ca2a6ce9d2ade6554
lzfdw <1256687835@qq.com>
2025-01-15 20:23:31 +0800

    添加公共组件

--- src/main/java/com/example/antmall/AntMailApplication.java
+++ src/main/java/com/example/antmall/AntMailApplication.java
@@ -1,4 +1,4 @@
-package com.example.antmail;
+package com.example.antmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
