bdc153904121751f07e5e914eed81fb152b9ed10
lzfdw <1256687835@qq.com>
2025-01-16 15:56:19 +0800

    添加product模块

---
+++ src/main/java/com/example/antmall/business/product/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.example.antmall.business.product.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface ProductService extends IService<Product> {
+
+}
---
+++ src/main/java/com/example/antmall/business/product/service/impl/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.antmall.business.product.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> implements ProductService {
+
+}
